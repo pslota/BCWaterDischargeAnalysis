7060a069e946a7ac711a9e221199c775595729ee
Jon Goetz <JGOETZ@GOVBC>
2017-10-27 15:38:55 -0700

    small edit

--- R/compute.Q.stat.longterm.R
+++ R/compute.Q.stat.longterm.R
@@ -1,4 +1,4 @@
-#' @title Compute long-term annual (calendar or water year) summary statistics.
+#' @title Compute long-term monthly and annual (calendar or water year) summary statistics.
 #'
 #' @description Computes long-term summary statistics on \code{flow$Q} variable
 #'    between \code{start.year} and \code{end.year} inclusive for calendar or water years.
