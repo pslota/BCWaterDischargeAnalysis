904bb63b2084c15e27fdeb2fa2e07eb6d5a93bb8
jongoetz <jon.goetz@gov.bc.ca>
2017-10-28 13:24:00 -0700

    updates

--- testing.R
+++ testing.R
@@ -1,8 +1,4 @@
-#flow <- read.csv("example_data_08NM116.csv",stringsAsFactors = FALSE,col.names = c("Date","Q"))
-flow <- read.csv("08NM116 - daily discharge.csv",stringsAsFactors = FALSE)
-flow <- dplyr::filter(flow,Parameter=="FLOW")
-flow <- dplyr::select(flow,Date,Q=Value)
-flow <- dplyr::mutate(flow,Date=as.Date(Date))
+
 
 flow$Date <- as.Date(flow$Date)
 
@@ -11,6 +7,13 @@ flow$MonthNum  <- lubridate::month(flow$Date)
 flow$Month <- month.abb[flow$MonthNum]
 flow$WaterYear <- as.numeric(ifelse(flow$MonthNum>=10,flow$Year+1,flow$Year))
 
+flow.data <- flow
+start.year <- 1975
+end.year <- 2000
+water.year <- FALSE
+na.rm <- list(na.rm.global=FALSE)
+basin.area <- 10
+csv.nddigits <- 3
 
 #  Compute calendar year long-term stats
 Q.month.longterm <-   dplyr::summarize(dplyr::group_by(flow,Month),
@@ -55,13 +58,13 @@ long.term <- compute.Q.stat.longterm(
 
 
   annual
-  annual <- compute.Q.stat.annual(station.name='Carnation',
+  annual <- compute.Q.stat.annual(station.name='Carnation-CY',
                                   basin.area=100,
                                   #flow.data=flow,
-                                  HYDAT = "08NM116",
-                                  water.year = TRUE,
-                                  #start.year=1975,
-                                  #end.year=2000,
+                                  HYDAT = "08HB048",
+                                  water.year = FALSE,
+                                  start.year=1975,
+                                  end.year=2000,
                                   write.table=TRUE,        # write out statistics on calendar year
                                   write.transposed.table=TRUE,  # write out statistics in transposed format (cy & wy)
                                   write.summary.table=TRUE, # write out a summary of period of record
@@ -74,3 +77,4 @@ long.term <- compute.Q.stat.longterm(
                                   debug=FALSE
   )
 annual.test <- annual$Q.stat.annual
+####### double check the date of half flow dates
