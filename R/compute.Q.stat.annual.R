94486c9150771c0eacbf7bf08b52646367aad6c0
jongoetz <jon.goetz@gov.bc.ca>
2017-10-28 13:24:25 -0700

    fixed glitches in the matrix

--- R/compute.Q.stat.annual.R
+++ R/compute.Q.stat.annual.R
@@ -149,8 +149,11 @@ compute.Q.stat.annual <- function(station.name=NULL,
   #  Generate all dates between min and max dates and merge with flow
   #  data frame to generate any dates that were missing.
   #  This will automatically generate NA for the days that were not in the file
-  temp <- data.frame(Date=seq(as.Date(paste(lubridate::year(min(flow.data$Date))-1,'10-01',sep='-'), "%Y-%m-%d"),
-                              as.Date(paste(lubridate::year(max(flow.data$Date))  ,'12-31',sep='-'), '%Y-%m-%d'), 1))
+
+  #min.year <- lubridate::year(min(flow.data$Date))
+  #max.year <- lubridate::year(max(flow.data$Date))
+  temp <- data.frame(Date=seq(as.Date(paste(start.year-1,'01-01',sep='-'), "%Y-%m-%d"),
+                              as.Date(paste(end.year  ,'12-31',sep='-'), '%Y-%m-%d'), 1))
   flow.data <- merge(flow.data, temp, all.y=TRUE)
 
   #  create the year (annual and water), seasonal, and month variables
@@ -193,7 +196,7 @@ compute.Q.stat.annual <- function(station.name=NULL,
   if (!is.numeric(end.year)) {end.year <- max(flow.data$AnalysisYear)}
   if(! (start.year <= end.year))    {stop("start.year must be less than end.year")}
 
-  flow.data <- dplyr::filter(flow.data, AnalysisYear >= start.year & AnalysisYear <=end.year)
+
 
 
 
@@ -223,16 +226,16 @@ compute.Q.stat.annual <- function(station.name=NULL,
   Q.stat.lowflow <-   dplyr::summarize(dplyr::group_by(flow.data,AnalysisYear),
                                        MIN_01Day    = min(Q, na.rm=na.rm$na.rm.global),	      # CY Min Daily Q
                                        MINDOY_01Day = ifelse(is.na(MIN_01Day),NA,
-                                                             DayofYear[which.min(Q)]),# Date of CY Min Daily Q
-                                       MIN_03Day    = min(Q, na.rm=na.rm$na.rm.global),	      # CY Min Daily Q
+                                                             AnalysisDoY[which.min(Q)]),# Date of CY Min Daily Q
+                                       MIN_03Day    = min(Q.03DAvg, na.rm=na.rm$na.rm.global),	      # CY Min Daily Q
                                        MINDOY_03Day = ifelse(is.na(MIN_03Day),NA,
-                                                             DayofYear[which.min(Q.03DAvg)]),# Date of CY Min Daily Q
-                                       MIN_07Day    = min(Q, na.rm=na.rm$na.rm.global),	      # CY Min Daily Q
+                                                             AnalysisDoY[which.min(Q.03DAvg)]),# Date of CY Min Daily Q
+                                       MIN_07Day    = min(Q.07DAvg, na.rm=na.rm$na.rm.global),	      # CY Min Daily Q
                                        MINDOY_07Day = ifelse(is.na(MIN_07Day),NA,
-                                                             DayofYear[which.min(Q.07DAvg)]),# Date of CY Min Daily Q
-                                       MIN_30Day    = min(Q, na.rm=na.rm$na.rm.global),	      # CY Min Daily Q
+                                                             AnalysisDoY[which.min(Q.07DAvg)]),# Date of CY Min Daily Q
+                                       MIN_30Day    = min(Q.30DAvg, na.rm=na.rm$na.rm.global),	      # CY Min Daily Q
                                        MINDOY_30Day = ifelse(is.na(MIN_30Day),NA,
-                                                             DayofYear[which.min(Q.30DAvg)]))# Date of CY Min Daily Q
+                                                             AnalysisDoY[which.min(Q.30DAvg)]))# Date of CY Min Daily Q
   Q.stat.lowflow <-   dplyr::rename(Q.stat.lowflow,Year=AnalysisYear)
 
   Q.stat.annual <-   dplyr::summarize(dplyr::group_by(flow.data,AnalysisYear),
@@ -268,6 +271,7 @@ compute.Q.stat.annual <- function(station.name=NULL,
   Q.stat.seasons2 <- dplyr::select(Q.stat.seasons2,-Seasons2,-stat)
   Q.stat.seasons2 <- tidyr::spread(Q.stat.seasons2,title,value)
   Q.stat.seasons2 <- dplyr::rename(Q.stat.seasons2,Year=WaterYear)
+  Q.stat.seasons2 <- dplyr::filter(Q.stat.seasons2, Year >= start.year & Year <= end.year)
 
   # monthly
   Q.stat.month <- dplyr::summarize(dplyr::group_by(flow.data,AnalysisYear,MonthText),
@@ -304,7 +308,12 @@ compute.Q.stat.annual <- function(station.name=NULL,
   Q.stat <- merge(Q.stat,Q.outside.quant,by="Year",all = TRUE)
   Q.stat <- tidyr::gather(Q.stat,Stat,Value,2:ncol(Q.stat))
   Q.stat <- dplyr::mutate(Q.stat,Stat=paste0(ifelse(water.year,paste("WY_"),paste("CY_")),Stat))
+  col.order <- c("Year",unique(Q.stat$Stat)) # to keep the same order as merged when spread below
   Q.stat <- tidyr::spread(Q.stat,Stat,Value)
+  Q.stat <- Q.stat[,col.order]
+
+  Q.stat <- dplyr::filter(Q.stat, Year >= start.year & Year <= end.year)
+  Q.stat <- dplyr::filter(Q.stat, Year >= min.year & Year <=max.year)
 
 
   ## Write the files
@@ -345,20 +354,37 @@ compute.Q.stat.annual <- function(station.name=NULL,
   }
 
   # Write out the low flow summary
+  # all mins and acutal dates
+  Q.lowflows <- tidyr::gather(Q.stat.lowflow,Stat,Value,2:ncol(Q.stat.lowflow))
+  Q.lowflows <- dplyr::mutate(Q.lowflows,Stat=paste0(ifelse(water.year,paste("WY_"),paste("CY_")),Stat))
+  col.order <- c("Year",unique(Q.lowflows$Stat)) # to keep the same order as merged when spread below
+  Q.lowflows <- tidyr::spread(Q.lowflows,Stat,Value)
+
+  if (water.year) {
+  Q.lowflows <- dplyr::mutate(Q.lowflows,
+                              WY_MINDate_01Day = as.Date(WY_MINDOY_01Day-1, origin=as.Date(paste0(Year-1,"-10-01"))),
+                              WY_MINDate_03Day = as.Date(WY_MINDOY_03Day-1, origin=as.Date(paste0(Year-1,"-10-01"))),
+                              WY_MINDate_07Day = as.Date(WY_MINDOY_07Day-1, origin=as.Date(paste0(Year-1,"-10-01"))),
+                              WY_MINDate_30Day = as.Date(WY_MINDOY_30Day-1, origin=as.Date(paste0(Year-1,"-10-01"))) )
+  } else {
+  Q.lowflows <- dplyr::mutate(Q.lowflows,
+                              CY_MINDate_01Day = as.Date(CY_MINDOY_01Day-1, origin=as.Date(paste0(Year,"-01-01"))),
+                              CY_MINDate_03Day = as.Date(CY_MINDOY_03Day-1, origin=as.Date(paste0(Year,"-01-01"))),
+                              CY_MINDate_07Day = as.Date(CY_MINDOY_07Day-1, origin=as.Date(paste0(Year,"-01-01"))),
+                              CY_MINDate_30Day = as.Date(CY_MINDOY_30Day-1, origin=as.Date(paste0(Year,"-01-01"))) )
+  }
+  Q.lowflows <- dplyr::select(Q.lowflows,Year,
+                              dplyr::contains("_01"),dplyr::contains("_03"),
+                              dplyr::contains("_07"),dplyr::contains("_30"))
+  Q.lowflows <- dplyr::filter(Q.lowflows, Year >= start.year & Year <= end.year)
+  Q.lowflows <- dplyr::filter(Q.lowflows, Year >= min.year & Year <=max.year)
+
   file.lowflow.csv <- NA
   if(write.lowflow.table){
     file.lowflow.csv <- file.path(report.dir,paste(station.name,"-lowflow-summary.csv",sep=""))
-    # all mins and acutal dates
-    Q.lowflows <- round(Q.stat.lowflow, csv.nddigits)
-    Q.lowflows <- dplyr::mutate(Q.stat.lowflow,
-                                MINDate_01Day = as.Date(MINDOY_01Day, origin=as.Date(paste0(Year,"-01-01"))),
-                                MINDate_03Day = as.Date(MINDOY_03Day, origin=as.Date(paste0(Year,"-01-01"))),
-                                MINDate_07Day = as.Date(MINDOY_07Day, origin=as.Date(paste0(Year,"-01-01"))),
-                                MINDate_30Day = as.Date(MINDOY_30Day, origin=as.Date(paste0(Year,"-01-01"))) )
-    Q.lowflows <- dplyr::select(Q.lowflows,Year,
-                                dplyr::contains("_01"),dplyr::contains("_03"),
-                                dplyr::contains("_07"),dplyr::contains("_30"))
-    utils::write.csv(Q.lowflows, file=file.lowflow.csv, row.names=FALSE)
+    temp <- Q.lowflows
+    temp[,c(2,5,8,11)] = round(temp[,c(2,5,8,11)],3)
+    utils::write.csv(temp, file=file.lowflow.csv, row.names=FALSE)
   }
 
   ## Do some plotting
@@ -722,7 +748,7 @@ compute.Q.stat.annual <- function(station.name=NULL,
 
     grDevices::dev.off()
   }
-  return(list( station name= station.name,
+  return(list( "station name"= station.name,
                "year type"=ifelse(!water.year,"Calendar Year (Jan-Dec)","Water Year (Oct-Sep)"),
                "year range"=paste0(start.year," - ",end.year),
                Q.flow.summary=flow.sum,
